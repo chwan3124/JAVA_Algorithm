be78edaad4282275fdc1ff85169866b3cfc8d42e
chwan <rthfickro3@gmail.com>
2025-05-01 18:33:51 +0900

    [baekjoon][solve] q_1929

---
+++ src/소수/q_1929/Main.java
@@ -0,0 +1,35 @@
+package 소수.q_1929;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean[] isNotPrime = new boolean[n + 1];
+
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (!isNotPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
